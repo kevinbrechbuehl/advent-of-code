ab3e4db1a88b6e43c112fc2cf8d4790fb1bdfc50
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2022-12-10 20:50:43 +0100

    2021 - Day 1

---
+++ 2021/Day01/Program.cs
@@ -0,0 +1,32 @@
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+
+int Total(int take)
+{
+    var sum = 0;
+    var previous = input.Take(take).Select(int.Parse).Sum();
+    for (var i = 1; i < input.Length; i++)
+    {
+        var current = input.Skip(i).Take(take).Select(int.Parse).Sum();
+        if (current > previous)
+        {
+            sum++;
+        }
+
+        previous = current;
+    }
+
+    return sum;
+}
+
+void Puzzle1()
+{
+    Console.WriteLine($"Puzzle 1: {Total(1)}");
+}
+
+void Puzzle2()
+{
+    Console.WriteLine($"Puzzle 2: {Total(3)}");
+}
+
+Puzzle1();
+Puzzle2();
