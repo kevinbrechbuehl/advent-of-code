322f0a5a66f4f3760d81dcedac37db11a25b19f7
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2023-01-07 20:40:32 +0100

    2022 - Day 6 Cleanup

--- 2022/06/puzzle.py
+++ 2022/06/puzzle.py
@@ -1,22 +1,32 @@
-with open("input.txt", "r") as f:
-    input = f.read()
+# --- Day 6: Tuning Trouble ---
+# https://adventofcode.com/2022/day/6
 
-def find_marker(char_count):
+import sys
+sys.path.insert(0, "..")
+
+import utils
+
+def part1() -> int:
+    return find_marker(4)
+
+def part2() -> int:
+    return find_marker(14)
+
+def find_marker(char_count: int) -> int:
+    input = utils.get_input()
     marker = char_count
     buffer = list(input[:char_count])
+
     for char in input[char_count:]:
         if len(set(buffer)) == char_count:
             return marker
+
         marker += 1
         buffer = buffer[1:] + [char]
 
-def puzzle1():
-    marker = find_marker(4)
-    print(f"Puzzle 1: {marker}")
+    return -1
 
-def puzzle2():
-    marker = find_marker(14)
-    print(f"Puzzle 2: {marker}")
+utils.execute([ part1, part2 ])
 
-puzzle1()
-puzzle2()
+# Part 1: 1538, took 11ms
+# Part 2: 2315, took 11ms
