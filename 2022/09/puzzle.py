82d2ee1fbb2dc4c83645385036e65832bd5ee1ef
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2023-01-11 20:53:31 +0100

    2022 - Day 9 Cleanup

--- 2022/09/puzzle.py
+++ 2022/09/puzzle.py
@@ -1,25 +1,27 @@
-with open("input.txt", "r") as f:
-    input = f.read().split("\n")
+# --- Day 9: Rope Bridge ---
+# https://adventofcode.com/2022/day/9
 
-def get_move(direction):
-    return {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}[direction]
+import sys
+sys.path.insert(0, "..")
 
-def follow_knot(previous, current):
-    distance_x = previous[0] - current[0]
-    if (abs(distance_x) > 1):
-        x = 1 if previous[0] > current[0] else -1
-        y = 0 if previous[1] == current[1] else 1 if previous[1] > current[1] else -1
-        return (current[0] + x, current[1] + y)
+import utils
 
-    distance_y = previous[1] - current[1]
-    if (abs(distance_y) > 1):
-        x = 0 if previous[0] == current[0] else 1 if previous[0] > current[0] else -1
-        y = 1 if previous[1] > current[1] else -1
-        return (current[0] + x, current[1] + y)
+moves = {
+    "R": (1, 0),
+    "L": (-1, 0),
+    "U": (0, 1),
+    "D": (0, -1)
+}
 
-    return current
+def part1() -> int:
+    return play(2)
 
-def play(number_of_knots):
+def part2() -> int:
+    return play(10)
+
+def play(number_of_knots: int) -> int:
+    input = utils.get_input_by_line()
+
     visited = set()
     knots = [(0, 0) for _ in range(number_of_knots)]
     
@@ -29,8 +31,10 @@ def play(number_of_knots):
         move = get_move(direction)
 
         for _ in range(distance):
+            # Move head
             knots[0] = (knots[0][0] + move[0], knots[0][1] + move[1])
 
+            # Each other knot should follow the previous
             for i in range(1, number_of_knots):
                 knots[i] = follow_knot(knots[i-1], knots[i])
                 
@@ -38,13 +42,25 @@ def play(number_of_knots):
 
     return len(visited)
 
-def puzzle1():
-    visited = play(2)
-    print(f"Puzzle 1: {visited}")
+def get_move(direction: str) -> tuple[int, int]:
+    return moves[direction]
 
-def puzzle2():
-    visited = play(10)
-    print(f"Puzzle 2: {visited}")
+def follow_knot(previous: tuple[int, int], current: tuple[int, int]) -> tuple[int, int]:
+    distance_x = previous[0] - current[0]
+    if (abs(distance_x) > 1):
+        delta_x = 1 if previous[0] > current[0] else -1
+        delta_y = 0 if previous[1] == current[1] else 1 if previous[1] > current[1] else -1
+        return (current[0] + delta_x, current[1] + delta_y)
 
-puzzle1()
-puzzle2()
+    distance_y = previous[1] - current[1]
+    if (abs(distance_y) > 1):
+        delta_x = 0 if previous[0] == current[0] else 1 if previous[0] > current[0] else -1
+        delta_y = 1 if previous[1] > current[1] else -1
+        return (current[0] + delta_x, current[1] + delta_y)
+
+    return current
+
+utils.execute([ part1, part2 ])
+
+# Part 1: 5907, took 27ms
+# Part 2: 2303, took 65ms
