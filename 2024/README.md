3dafb05b48e61bd569c5314a25ef6131aa131fd6
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2024-11-30 19:39:29 +0100

    Prepare for Advent of Code 2024

---
+++ 2024/README.md
@@ -0,0 +1,28 @@
+# Advent of Code 2024
+
+Code puzzle implementations for the [Advent of Code 2024](https://adventofcode.com/2024). Last year was pretty funny, so I'm solving the puzzles with [TypeScript](https://www.typescriptlang.org) again.
+
+As a prerequisite, you need to globally install `ts-node`:
+
+```
+npm install -g ts-node
+```
+
+You can run a puzzle by executing the following command in the specific folder:
+
+```
+ts-node puzzle.ts
+```
+
+If you want to run against the sample input, you can execute the following command:
+
+```
+ts-node puzzle.ts -- sample
+```
+
+## Input Data
+
+It's not allowed to share the input data. Therefore, you have to copy the input data from the website and add them to the folder of the specific day:
+
+- Real input into `input.txt`
+- Sample input into `sample.txt`
---
+++ 2024/utils.ts
@@ -0,0 +1,48 @@
+// @ts-ignore
+import { readFileSync } from "fs";
+
+export const getInputByLine = (fileName?: string): string[] => {
+  return getInput(fileName).replace(/\r/g, "").split("\n");
+};
+
+export const getInputByGroupOfLines = (fileName?: string): string[][] => {
+  return getInput(fileName)
+    .replace(/\r/g, "")
+    .split("\n\n")
+    .map((group) => group.split("\n"));
+};
+
+export const getInput = (fileName?: string): string => {
+  if (!fileName) {
+    fileName = isSampleInput() ? "sample.txt" : "input.txt";
+  }
+
+  return readFileSync(fileName).toString();
+};
+
+export const isSampleInput = (): boolean => {
+  // @ts-ignore
+  return process.argv.some((arg: string) => arg === "sample");
+};
+
+export const execute = (parts: (() => number)[]): void => {
+  parts.forEach((part, i) => {
+    const start = new Date();
+
+    const result = part();
+
+    const end = new Date();
+    const duration = formatDuration(start, end);
+
+    console.log(`Part ${i + 1}: ${result}, took ${duration}`);
+  });
+};
+
+export const formatDuration = (start: Date, end: Date): string => {
+  const duration = end.valueOf() - start.valueOf(); // in milliseconds
+  if (duration < 1000) {
+    return `${duration}ms`;
+  } else {
+    return `${duration / 1000}s`;
+  }
+};
