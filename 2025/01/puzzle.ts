f60cc8e3a7c7dd8130ff9b4b8211b2a4d19df516
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2025-12-01 13:02:19 +0100

    2025 - Day 1

---
+++ 2025/01/puzzle.ts
@@ -0,0 +1,48 @@
+﻿// ---- Day 1: Secret Entrance ---
+// https://adventofcode.com/2025/day/1
+
+import { getInputByLine, execute } from "../utils";
+
+const part1 = (): number => {
+  let password = 0;
+  let dial = 50;
+
+  getInputByLine().forEach((line) => {
+    const direction = line[0];
+    const distance = parseInt(line.slice(1), 10);
+
+    const sign = direction === "L" ? -1 : 1;
+    dial = (dial + sign * distance + 100) % 100;
+
+    if (dial === 0) {
+      password++;
+    }
+  });
+
+  return password;
+};
+
+const part2 = (): number => {
+  let password = 0;
+  let dial = 50;
+
+  getInputByLine().forEach((line) => {
+    const direction = line[0];
+    const distance = parseInt(line.slice(1), 10);
+
+    const step = direction === "L" ? -1 : 1;
+    for (let i = 0; i < distance; i++) {
+      dial = (dial + step + 100) % 100;
+      if (dial === 0) {
+        password++;
+      }
+    }
+  });
+
+  return password;
+};
+
+execute([part1, part2]);
+
+// Part 1: 1026, took 1ms
+// Part 2: 5923, took 3ms
